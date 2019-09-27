012a0f44444e370a37200c77ebf8d7a1bb6ec87c
Tom Stuart <tom.stuart@futurelearn.com>
2019-09-27 09:03:15 +0100

    Implement anagramsOf

--- javascript/lib/anagramsOf.js
+++ javascript/lib/anagramsOf.js
@@ -1,5 +1,16 @@
 const anagramsOf = string => {
-  // TODO
+  if (string.length === 0) {
+    return [''];
+  } else {
+    let results = [];
+    for (let i = 0; i < string.length; ++i) {
+      const letter = string[i];
+      const rest = string.slice(0, i) + string.slice(i + 1, string.length);
+      const anagrams = anagramsOf(rest).map(anagram => letter + anagram);
+      results = results.concat(anagrams);
+    }
+    return results;
+  }
 }
 
 export default anagramsOf;
--- ruby/lib/anagrams_of.rb
+++ ruby/lib/anagrams_of.rb
@@ -1,3 +1,11 @@
 def anagrams_of(string)
-  # TODO
+  if string.length.zero?
+    ['']
+  else
+    (0...string.length).flat_map do |i|
+      letter = string[i]
+      rest = string.slice(0, i) + string.slice(i + 1, string.length)
+      anagrams_of(rest).map { |anagram| letter + anagram }
+    end
+  end
 end
