aadec11d74c8db923a32649c1307123787cf6fb1
Tom Stuart <tom.stuart@futurelearn.com>
2019-09-27 09:03:18 +0100

    Add iterative sumUpTo

--- javascript/lib/sumUpTo.js
+++ javascript/lib/sumUpTo.js
@@ -1,3 +1,15 @@
+// const sumUpTo = number => {
+//   let result = 0;
+
+//   let i = 1;
+//   while (i <= number) {
+//     result += i;
+//     i++;
+//   }
+
+//   return result;
+// };
+
 const sumUpTo = number => {
   if (number === 0) {
     return 0;
--- ruby/lib/sum_up_to.rb
+++ ruby/lib/sum_up_to.rb
@@ -1,3 +1,15 @@
+# def sum_up_to(number)
+#   result = 0
+
+#   i = 0
+#   while i <= number
+#     result += i
+#     i += 1
+#   end
+
+#   result
+# end
+
 def sum_up_to(number)
   if number.zero?
     0
