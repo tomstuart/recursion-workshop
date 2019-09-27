680c62c12d7491aaf068c44461c2a5ff6bf0c89e
Tom Stuart <tom.stuart@futurelearn.com>
2019-09-27 09:03:14 +0100

    Implement fibonacci

--- javascript/lib/fibonacci.js
+++ javascript/lib/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = number => {
-  // TODO
+  switch (number) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      return fibonacci(number - 1) + fibonacci(number - 2);
+  }
 };
 
 export default fibonacci;
--- ruby/lib/fibonacci.rb
+++ ruby/lib/fibonacci.rb
@@ -1,3 +1,10 @@
 def fibonacci(number)
-  # TODO
+  case number
+  when 0
+    0
+  when 1
+    1
+  else
+    fibonacci(number - 1) + fibonacci(number - 2)
+  end
 end
