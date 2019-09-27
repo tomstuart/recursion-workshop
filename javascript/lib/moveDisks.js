678de3dcd6b84e15ab95ab48ba30c9f6ffc90781
Tom Stuart <tom.stuart@futurelearn.com>
2019-09-27 09:03:16 +0100

    Implement moveDisks

--- javascript/lib/moveDisks.js
+++ javascript/lib/moveDisks.js
@@ -1,5 +1,15 @@
 const moveDisks = (tower, count, from, to) => {
-  // TODO
+  if (count === 1) {
+    return tower.move(from, to);
+  } else {
+    const stage = tower.bases.find(base => ![from, to].includes(base.id)).id;
+
+    let result = tower;
+    result = moveDisks(result, count - 1, from, stage);
+    result = moveDisks(result, 1, from, to);
+    result = moveDisks(result, count - 1, stage, to);
+    return result;
+  }
 };
 
 export default moveDisks;
--- ruby/lib/move_disks.rb
+++ ruby/lib/move_disks.rb
@@ -1,3 +1,11 @@
 def move_disks(tower, count, from, to)
-  # TODO
+  if count == 1
+    tower.move(from, to)
+  else
+    stage = tower.bases.detect { |base| ![from, to].include?(base.id) }.id
+    tower = move_disks(tower, count - 1, from, stage)
+    tower = move_disks(tower, 1, from, to)
+    tower = move_disks(tower, count - 1, stage, to)
+    tower
+  end
 end
