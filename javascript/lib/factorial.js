c6cea701f658edf1979c475b705637e9fd2dcaa0
Tom Stuart <tom.stuart@futurelearn.com>
2019-09-27 09:03:19 +0100

    Add iterative factorial

--- javascript/lib/factorial.js
+++ javascript/lib/factorial.js
@@ -1,3 +1,15 @@
+// const factorial = number => {
+//   let result = 1;
+
+//   let i = 1;
+//   while (i <= number) {
+//     result *= i;
+//     i++;
+//   }
+
+//   return result
+// };
+
 const factorial = number => {
   if (number === 0) {
     return 1;
--- ruby/lib/factorial.rb
+++ ruby/lib/factorial.rb
@@ -1,3 +1,15 @@
+# def factorial(number)
+#   result = 1
+
+#   i = 1
+#   while i <= number
+#     result *= i
+#     i += 1
+#   end
+
+#   result
+# end
+
 def factorial(number)
   if number.zero?
     1
